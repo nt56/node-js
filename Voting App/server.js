f5897b9eb219d115d710489fdde2ee5296c0caf6
nt_56 <nagutirth2121@gmail.com>
2024-11-11 09:24:18 +0530

    voting app project setup and add

---
+++ Voting App/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+const db = require("./db");
+require("dotenv").config();
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 3000;
+
+// Import the router files
+// Use the routers
+
+app.listen(PORT, () => {
+  console.log("listening on port 3000");
+});
