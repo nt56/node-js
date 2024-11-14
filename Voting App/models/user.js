6303c70e7ca446fbf6b120c4d09d4d31626306ea
nt_56 <nagutirth2121@gmail.com>
2024-11-14 13:34:19 +0530

    project completed

--- Voting App/models/user.js
+++ Voting App/models/user.js
@@ -32,13 +32,43 @@ const userSchema = new mongoose.Schema({
   role: {
     type: String,
     enum: ["voter", "admin"],
-    default: voter,
+    default: "voter",
   },
   isVoted: {
     type: Boolean,
     default: false,
   },
 });
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  // Hash the password only if it has been modified (or is new)
+  if (!user.isModified("password")) return next();
+  try {
+    // hash password generation
+    const salt = await bcrypt.genSalt(10);
+
+    // hash password
+    const hashedPassword = await bcrypt.hash(user.password, salt);
+
+    // Override the plain password with the hashed one
+    user.password = hashedPassword;
+    next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  try {
+    // Use bcrypt to compare the provided password with the hashed password
+    const isMatch = await bcrypt.compare(candidatePassword, this.password);
+    return isMatch;
+  } catch (err) {
+    throw err;
+  }
+};
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
--- Voting App/server.js
+++ Voting App/server.js
@@ -10,7 +10,12 @@ app.use(bodyParser.json());
 const PORT = process.env.PORT || 3000;
 
 // Import the router files
+const userRoutes = require("./routes/userRoutes");
+const candidateRoutes = require("./routes/candidateRoutes");
+
 // Use the routers
+app.use("/user", userRoutes);
+app.use("/candidate", candidateRoutes);
 
 app.listen(PORT, () => {
   console.log("listening on port 3000");
